4a22acf6bfa172180f0c9682dc8ec13a33ad0982
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-12-04 02:51:07 +0900

    Initial Spring Boot

---
+++ src/test/java/com/goormthon/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goormthon.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
