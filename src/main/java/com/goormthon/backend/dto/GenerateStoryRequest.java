a178b1ffe284170a27f0ec1715f22f0faf6b43a9
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-12-05 05:54:30 +0900

    fix: fix dto

--- src/main/java/com/goormthon/backend/dto/GenerateStoryRequest.java
+++ src/main/java/com/goormthon/backend/dto/GenerateStoryRequest.java
@@ -12,11 +12,6 @@ public class GenerateStoryRequest {
     private String address;
 
     private String text1;
-    private String imageUrl1;
-
     private String text2;
-    private String imageUrl2;
-
     private String text3;
-    private String imageUrl3;
 }
