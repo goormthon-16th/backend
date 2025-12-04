2c4da9e637cc16a421dc6cb679097c8dc0e2619a
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-12-05 03:18:46 +0900

    fix: fix s3..

--- src/main/java/com/goormthon/backend/config/S3Config.java
+++ src/main/java/com/goormthon/backend/config/S3Config.java
@@ -14,12 +14,6 @@ public class S3Config {
     @Value("${cloud.aws.region.static}")
     private String region;
 
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
-
     @Bean
     public S3Client s3Client() {
 
